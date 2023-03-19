5f3dddc3b9591da83841447e426a375c1b9e71ba
kambrium <kambrium@users.noreply.github.com>
2023-03-19 21:15:41 +0100

    Config of server log

--- source/app.d
+++ source/app.d
@@ -2,7 +2,14 @@
 
 import dwttool.cli;
 
+import slf4d;
+import slf4d.default_provider;
+
 void main(string[] args)
-{ 
+{
+    // Configuration of server logging
+    auto provider = new shared DefaultProvider(false, Levels.ERROR);
+    configureLoggingProvider(provider);
+
     handleCli(args);
 }
